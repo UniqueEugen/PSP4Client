d0dcfd32cc499c9abda9b1c87b11d45e8f60b62a
UniqueEugen <112904113+UniqueEugen@users.noreply.github.com>
2023-10-27 23:09:22 +0300

    Git push

--- src/client/UDPClient.java
+++ src/client/UDPClient.java
@@ -1,2 +1,38 @@
-package client;public class UDPClient {
+package client;
+
+import java.net.*;
+import java.io.*;
+public class UDPClient {
+    private String DATA;
+    public void runClient() throws IOException {
+        DatagramSocket s = null;
+        try {
+            byte[] buf = new byte[64];
+            s = new DatagramSocket();
+
+            System.out.println("UDPClient: Started");
+            String COUNT_CMD = "COUNT";
+            DatagramPacket sendPacket = new DatagramPacket(COUNT_CMD.getBytes(), COUNT_CMD.getBytes().length, InetAddress.getByName("127.0.0.1"), 8001);
+            s.send(sendPacket);
+            PrinterScanner printScan = new PrinterScanner();
+            DATA = printScan.toString();
+            sendPacket = new DatagramPacket(DATA.getBytes(), DATA.getBytes().length, InetAddress.getByName("127.0.0.1"), 8001);
+            s.send(sendPacket);
+            DatagramPacket recvPacket = new DatagramPacket(buf, buf.length);//создание дейтаграммы для получения данных
+            s.receive(recvPacket);
+            String answer = new String(recvPacket.getData()).trim();
+            System.out.println("Answer is: " + answer);
+            byte[] quitCmd = {'Q', 'U', 'I', 'T'};
+            sendPacket.setData(quitCmd);//установить массив посылаемых данных
+            sendPacket.setLength(quitCmd.length);//установить длину посылаемых
+// данных
+            s.send(sendPacket); //послать данные серверу
+            System.out.println("UDPClient: Ended");
+        } finally {
+            if (s != null) {
+                s.close();//закрытие сокета клиента
+            }
+        }
+
+    }
 }
--- src/scan/Scan.java
+++ src/scan/Scan.java
@@ -1,2 +1,29 @@
-package scan;public class Scan {
+package scan;
+
+import java.util.Scanner;
+
+public class Scan {
+    private static final Scanner sc = new Scanner(System.in);
+    public static int intScan(){
+        int i;
+        while (true) {
+            try {
+                i = Integer.parseInt(sc.next());
+                return i;
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный ввод!");
+            }
+        }
+    }
+    public static double doubleScan(){
+        double i;
+        while (true) {
+            try {
+                i = Double.parseDouble(sc.next());
+                return i;
+            } catch (NumberFormatException e) {
+                System.out.println("Неверный ввод!");
+            }
+        }
+    }
 }
